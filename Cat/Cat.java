7adea16a7128ff82d691d3f203c6a76c09f98931
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-06-24 11:59:28 +0100

    file manager

--- Cat/Cat.java
+++ Cat/Cat.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 
 public class Cat {
     public static void cat(String[] args) throws IOException {
